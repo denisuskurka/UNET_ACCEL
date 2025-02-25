e8f29dd431e2c69ba247e8566c39377faee3391e
Denis Kurka <komarovec1@gmail.com>
2025-02-25 22:22:02 +0100

    fix board argument

--- hls4ml/unet_fz5/synth_z2.py
+++ hls4ml/unet_fz5/synth_z2.py
@@ -34,7 +34,7 @@ qmodel = tf.keras.models.load_model('quantized_cnn_model_final_128_20.h5', custo
 qmodel = strip_pruning(qmodel)
 
 # Then the QKeras model
-hls_config_q = hls4ml.utils.config_from_keras_model(qmodel, granularity='model', backend='VivadoAccelerator')
+hls_config_q = hls4ml.utils.config_from_keras_model(qmodel, granularity='model', backend='VivadoAccelerator', board='fz5')
 hls_config_q['Model']['ReuseFactor'] = 4
 hls_config_q['Model']['Precision'] = 'ap_fixed<32,8>'
 hls_config_q['Flows'] = ['vivadoaccelerator:fifo_depth_optimization']
