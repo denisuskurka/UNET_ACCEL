ac986f072aa0eb1e76e7c2d9900b258b7305a42f
Denis Kurka <komarovec1@gmail.com>
2025-04-13 15:06:37 +0200

    clean up

--- final_deployment/clean.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ./data
-rm ./flask_app.zip
--- final_deployment/start_dma_engine.sh
+++ final_deployment/start_dma_engine.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-nohup sudo ./dma_driver > dma_driver.log 2>&1 &
+
+# Name of the driver process
+DRIVER_NAME="dma_driver"
+
+# 1) Check if the driver is running
+pids=$(pgrep -f "$DRIVER_NAME")
+
+if [ -n "$pids" ]; then
+  echo "$DRIVER_NAME is running with PID(s): $pids"
+  echo "Killing it..."
+  sudo kill $pids
+  echo "Done."
+else
+  echo "$DRIVER_NAME not running. Launching it with nohup..."
+  nohup sudo ./$DRIVER_NAME > dma_driver.log 2>&1 &
+  echo "Started $DRIVER_NAME in background (logs in dma_driver.log)."
+fi
